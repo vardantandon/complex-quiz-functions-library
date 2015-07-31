95caa22d75ffe226c8b504d0453436d7c500cac0
Vardan Tandon <vardan.tandon.13@ucl.ac.uk>
2015-07-31 15:37:36 +0100

    Q2(b) update

--- Math2 Quizzes/Series/Final Version/Question 2(b)/Question_2.m
+++ Math2 Quizzes/Series/Final Version/Question 2(b)/Question_2.m
@@ -1,4 +1,3 @@
-
 file = fopen('Question_2.xml', 'w');
 fprintf(file,quiz_start());
 
@@ -9,6 +8,8 @@ b = randi([6 10],1,1)
 c = randi([1 5],1,1)
 d = randi([1 5],1,1)
 flag = randi([1 4],1,1)
+numb = randi([1 3],1,1)
+flag_1 = randi([1 3],1,1)
 
 if (flag == 1)
     c = -1*c
@@ -26,9 +27,21 @@ else if(flag == 2)
         end
     end
 end
-
+if (numb == 0)
 sum_1 = 1/(1-(1/a))
 sum_2 = 1/(1-(1/b))
+else if(numb == 1)
+        sum_1 = (1/a)/(1-(1/a))
+        sum_2 = (1/b)/(1-(1/b))
+    else if(numb == 2)
+        sum_1 = (1/a)^2/(1-(1/a))
+        sum_2 = (1/b)^2/(1-(1/b)) 
+        else
+        sum_1 = (1/a)^3/(1-(1/a))
+        sum_2 = (1/b)^3/(1-(1/b))   
+        end 
+    end 
+end
 
 sum = c*sum_1 + d*sum_2
 
@@ -53,20 +66,52 @@ end
 numString = cloze_numerical(sum, 0.05 , 'Good job!', 'Incorrect, try again');
 
 if (flag == 1 || flag ==3)
-  
- questionString = strcat('Evaluate the sum of the infinite series : <br>','$$\\small \\sum _{n=0}^{\\infty }\\:[',cString,'(\\frac{1}{',num2str(a),...
-    '})^n',dString,'(\\frac{1}{',num2str(b),'})^n]$$ <br>','<br>[Calculate upto 2 significant figures]<br>', ...
+ if (a==3||a==5)
+ questionString = strcat('Evaluate the sum of the infinite series : <br>','$$\\small \\sum _{n=',num2str(numb),'}^{\\infty }\\:[',cString,'(',num2str(a^2),...
+    ')^{-\\frac{n}{2}}',dString,'(\\frac{1}{',num2str(b),'})^n]$$ <br>','<br>[Calculate upto 2 significant figures]<br>', ...
  '<br>Answer : <br>', numString);
-
+ 
+ else if (b==9)
+  questionString = strcat('Evaluate the sum of the infinite series : <br>','$$\\small \\sum _{n=',num2str(numb),'}^{\\infty }\\:[',cString,'(\\frac{1}{',num2str(a),...
+    '})^n',dString,'(',num2str(sqrt(b)),')^{2n}]$$ <br>','<br>[Calculate upto 2 significant figures]<br>', ...
+ '<br>Answer : <br>', numString);   
+     else
+  questionString = strcat('Evaluate the sum of the infinite series : <br>','$$\\small \\sum _{n=',num2str(numb),'}^{\\infty }\\:[',cString,'(\\frac{1}{',num2str(a),...
+  '})^n',dString,'(\\frac{1}{',num2str(b),'})^n]$$ <br>','<br>[Calculate upto 2 significant figures]<br>', ...
+ '<br>Answer : <br>', numString); 
+         
+     end
+     
+     
+ end
+ 
  feedbackString = strcat('This contains two geometric series with first term a = 1 and common ratios $$\\small \\frac{1}{', num2str(a), '}$$ and $$\\small \\frac{1}{', num2str(b),...
      '}$$ respectively.<br>', 'Number of terms n = $$\\small \\infty$$ <br>', 'Sum = $$\\small \\frac{a(1 - r^{n})}{1 - r}$$ <br>', 'Sum =&nbsp;&nbsp;',num2str(sum));
 
-
+ feedbackString = strcat('This contains two geometric series where the following is given : <br>',...
+ 'First term : a<sub>1</sub> =','',' a<sub>2</sub> ','','[At n=0 , the first term for both the series will be to the power 0 which equals 1]<br>Common ratios : r<sub>1</sub> =&nbsp;$$\\small \\frac{1}{', num2str(a),'}$$ , ',...
+ 'r<sub>2</sub> =&nbsp;$$\\small \\frac{1}{', num2str(b),'}$$<br>','Number of terms(n) = $$\\small \\infty$$ <br><br>',...
+ 'The Sum of a Geometric series is given by :<br> $$\\small \\frac{a(1 - r^{n})}{1 - r}$$  where a is the first term , r is the common difference and n is the number of terms <br><br>',...
+ 'As n = $$\\small \\infty$$, Sum = $$\\small \\frac{a}{1 - r}$$<br>',...
+ 'Sum<sub>1</sub> = $$\\small \\frac{a}{1 - r_{1}}$$ ; Sum<sub>2</sub> = $$\\small \\frac{a}{1 - r_{2}}$$ <br>',...
+ 'Sum<sub>1</sub> =&nbsp;',num2str(sum_1), '&nbsp;&nbsp;Sum<sub>2</sub> =&nbsp;',num2str(sum_2),'<br><br>Total Sum = (',num2str(c),') * Sum<sub>1</sub> + (',num2str(d),') * Sum<sub>2</sub><br>Total Sum =&nbsp;',num2str(sum));
+ 
 else
-questionString = strcat('Evaluate the sum of the infinite series : <br>','$$\\small \\sum _{n=0}^{\\infty }\\:[',cString,'(\\frac{1}{',num2str(a),...
-    '})^n' ,'+',dString,'(\\frac{1}{',num2str(b),'})^n]$$ <br>','<br>[Calculate upto 2 significant figures]<br>', ...
+    
+    if (a==3||a==5)
+ questionString = strcat('Evaluate the sum of the infinite series : <br>','$$\\small \\sum _{n=',num2str(numb),'}^{\\infty }\\:[',cString,'(',num2str(a^2),...
+    ')^{-\\frac{n}{2}}','+',dString,'(\\frac{1}{',num2str(b),'})^n]$$ <br>','<br>[Calculate upto 2 significant figures]<br>', ...
  '<br>Answer : <br>', numString);
-
+    else if(b==9)
+  questionString = strcat('Evaluate the sum of the infinite series : <br>','$$\\small \\sum _{n=',num2str(numb),'}^{\\infty }\\:[',cString,'(\\frac{1}{',num2str(a),...
+    '})^n','+',dString,'(',num2str(sqrt(b)),')^{2n}]$$ <br>','<br>[Calculate upto 2 significant figures]<br>', ...
+ '<br>Answer : <br>', numString);
+        else
+    questionString = strcat('Evaluate the sum of the infinite series : <br>','$$\\small \\sum _{n=',num2str(numb),'}^{\\infty }\\:[',cString,'(\\frac{1}{',num2str(a),...
+    '})^n','+',dString,'(\\frac{1}{',num2str(b),'})^n]$$ <br>','<br>[Calculate upto 2 significant figures]<br>', ...
+ '<br>Answer : <br>', numString);
+        end 
+    end
 feedbackString = strcat('This contains two geometric series with first term a = 1 and common ratios $$\\small \\frac{1}{', num2str(a), '}$$ and $$\\small \\frac{1}{', num2str(b),...
      '}$$ respectively.<br>', 'Number of terms n = $$ \\small \\infty$$ <br>', 'Sum = $$\\small \\frac{a(1 - r^{n})}{1 - r}$$ <br>', 'Sum =&nbsp;&nbsp;',num2str(sum));
  
